5edfba2b04d42a0ef9f0eeb91501b71c782f306a
NguyenThiNho <nho29042002@gmail.com>
2023-10-18 17:10:21 +0700

    Add for.java

---
+++ for.java
@@ -0,0 +1,8 @@
+public class for {
+    public static void main(String[] args) {
+    // in các số từ 1 đến 10
+    for(int i = 1; i <= 10; i++) {
+        System.out.println(i);
+        }
+    }
+}
